cf4c5ed2a5aa8a6b4a9224a3893a193f92735521
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-09-24 20:31:29 -0300

    novamente css

--- frontend/src/components/Sobre.vue
+++ frontend/src/components/Sobre.vue
@@ -4,7 +4,7 @@
     <NavBarSobre></NavBarSobre>
     <div class="container">
         <div class="intro-text1">
-          <div class="imgpos">
+          <div class="inferno">
             <div class="card"  style="width: 40rem;">
               <div class="card-body"  >
                 <p class="text-justify">É uma oportunidade para empresas anunciarem suas vagas e para as pessoas conseguirem buscar uma vaga de emprego na região, fazendo isso de forma que o processo de preenchimento do cargo e seleção dos candidatos seja limpo e justo para as organizações e seus interessados. Junto da Associação Comercial de Corumbá, onde as empresas que são suas associadas divulgam seus cargos e funções disponíveis.</p>
