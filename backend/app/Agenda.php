aeeb1001f07ef76a6e885c28ad4050f342725ca1
Luiza <luizamazo@hotmail.com>
2019-09-25 22:38:56 -0300

    ediçao ai sei la

--- backend/app/Agenda.php
+++ backend/app/Agenda.php
@@ -9,4 +9,8 @@ class Agenda extends Model
     protected $fillable = [
 		'data', 'hora', 'observacao', 'candidatura_id'
 	];
+
+	public function candidatura(){
+		return $this->hasOne(Candidatura::class, 'candidatura_id');
+	}
 }
