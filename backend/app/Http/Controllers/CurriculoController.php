b0290cb1eed3353ac0a90a2278d0d13f281c7a00
krdg10 <krdg10@live.com>
2019-09-27 10:29:57 -0400

    curriculo controller com todas as validações decididas até então

--- backend/app/Http/Controllers/CurriculoController.php
+++ backend/app/Http/Controllers/CurriculoController.php
@@ -15,57 +15,175 @@ class CurriculoController extends Controller
         
     public function store(Request $request){
       
-        if(!$request->rua){
-            $error[] = 'Insira a rua!';
+        if(!$request->nome){
+            $error[] = 'Insira um nome!';
         }
-        if(!$request->bairro){
-            $error[] = 'Insira o bairro!';
+        else{
+            if(strlen($request->nome)>50){
+                $error[] = 'Insira um nome com no máximo 50 caracteres!';
+            }
         }
-        if(!$request->cidade){
-            $error[] = 'Insira a cidade!';
-        }
-        if(!$request->estado){
-            $error[] = 'Insira o estado!';
-        }
-        if(!$request->cep){
-            $error[] = 'Insira o CEP!';
-        }
-        if(!$request->pais){
-            $error[] = 'Insira o país!';
-        }
-        if(!$request->objetivos){
-            $error[] = 'Insira seus objetivos!';
-        }
-        if(!$request->area){
-            $error[] = 'Insira sua área!';
-        }
-        if(!$request->pretensao){
-            $error[] = 'Insira sua pretensão salarial!';
-        }
-        if(!$request->qualificacoes){
-            $error[] = 'Insira suas qualificações!';
-        }
-        if(!$request->historicoProfissional){
-            $error[] = 'Insira seu histórico!';
-        }
-        if(!$request->escolaridade){
-            $error[] = 'Insira sua escolaridade!';
-        }
+
         if(!$request->nascimento){
             $error[] = 'Insira sua data de nascimento!';
         }
-        if(!$request->genero){
-            $error[] = 'Insira seu genero!';
-        }
+
         if(!$request->estadoCivil){
             $error[] = 'Insira seu estado civil!';
         }
-        /*if(CurriculoController::celular($request->fixo)==false){
-            $error[] = 'Digite número fixo válido!';
+
+        if(!$request->genero){
+            $error[] = 'Insira seu genero!';
         }
-        if(CurriculoController::celular($request->celular)==false){
-            $error[] = 'Digite celular válido!';
-        } */
+
+        if($request->linkedin){
+            if(strlen($request->linkedin)>50){
+                $error[] = 'Insira um linkedin com no máximo 50 caracteres!';
+            }
+        }
+
+        if($request->facebook){
+            if(strlen($request->facebook)>50){
+                $error[] = 'Insira um facebook com no máximo 50 caracteres!';
+            }
+        }
+
+        if($request->twitter){
+            if(strlen($request->twitter)>50){
+                $error[] = 'Insira um twitter com no máximo 50 caracteres!';
+            }
+        }
+
+        if($request->site){
+            if(strlen($request->site)>50){
+                $error[] = 'Insira o site com no máximo 50 caracteres!';
+            }
+        }
+
+        if($request->outraRede){
+            if(strlen($request->outraRede)>50){
+                $error[] = 'Insira sua outra rede com no máximo 50 caracteres!';
+            }
+        }
+
+        if($request->emailAlt){
+            if(!filter_var($request->emailAlt, FILTER_VALIDATE_EMAIL)){
+                $error[] = 'Insira email válido no campo email alternativo!';
+            }
+            else if(strlen($request->emailAlt)>50){
+                $error[] = 'Insira seu email alternativo com no máximo 50 caracteres!';
+            }
+        }
+
+        if(!$request->pais){
+            $error[] = 'Insira o país!';
+        }
+
+        if(!$request->estado){
+            $error[] = 'Insira o estado!';
+        }
+
+        if(!$request->fixo){
+            $error[] = 'Insira número fixo!';
+        }
+        else{
+            /*if(CurriculoController::celular($request->fixo)==false){
+                $error[] = 'Digite número fixo válido!';
+            }*/
+        }
+        
+        if(!$request->celular){
+            $error[] = 'Insira número de celular!';
+        }
+        else{ 
+            /*if(CurriculoController::celular($request->celular)==false){
+                $error[] = 'Digite celular válido!';
+            } */
+        }
+        
+        if(!$request->rua){
+            $error[] = 'Insira a rua!';
+        }
+        else {
+            if(strlen($request->rua)>50){
+                $error[] = 'Insira sua rua com no máximo 50 caracteres!';
+            }
+        }
+
+        if(!$request->bairro){
+            $error[] = 'Insira o bairro!';
+        }
+        else {
+            if(strlen($request->bairro)>50){
+                $error[] = 'Insira seu bairro com no máximo 50 caracteres!';
+            }
+        }
+
+        if(!$request->cidade){
+            $error[] = 'Insira a cidade!';
+        }
+        else {
+            if(strlen($request->cidade)>50){
+                $error[] = 'Insira sua cidade com no máximo 50 caracteres!';
+            }
+        }
+        
+        if(!$request->cep){
+            $error[] = 'Insira o CEP!';
+        }
+        else{
+            /*if(CurriculoController::validarCep($request->cep)==false){
+                $error[] = 'Digite CEP válido!';
+            }*/
+        }
+        
+        if(!$request->objetivos){
+            $error[] = 'Insira seus objetivos!';
+        }
+        else{
+            if(strlen($request->objetivos)>500){
+                $error[] = 'Insira seus objetivos com no máximo 500 caracteres!';
+            }
+        }
+
+        if(!$request->area){
+            $error[] = 'Insira sua área!';
+        }
+
+        if(!$request->pretensao){
+            $error[] = 'Insira sua pretensão salarial!';
+        }
+        else{
+            if (!is_numeric($request->pretensao)){
+                $error[] = 'Insira sua pretensão salarial apenas com números!';
+            }
+            if($request->pretensao<1){
+                $error[] = 'Insira pretensão salarial com valor maior que 1!';
+            }
+        }
+
+        if(!$request->escolaridade){
+            $error[] = 'Insira sua escolaridade!';
+        }
+
+        if(!$request->qualificacoes){
+            $error[] = 'Insira suas qualificações!';
+        }
+        else{
+            if(strlen($request->qualificacoes)>500){
+                $error[] = 'Insira suas qualificações com no máximo 500 caracteres!';
+            }
+        }
+
+        if(!$request->historicoProfissional){
+            $error[] = 'Insira seu histórico!';
+        }
+        else{
+            if(strlen($request->historicoProfissional)>500){
+                $error[] = 'Insira seu histórifo profissional com no máximo 500 caracteres!';
+            }
+        }
+       
         if(isset($error)){
             return Response::json([
             'error' => $error
@@ -214,6 +332,20 @@ class CurriculoController extends Controller
             return true;
         }else{
             return false;
+        }
+    }
+    function validarCep($cep) {
+        // retira espacos em branco
+        $cep = trim($cep);
+        // expressao regular para avaliar o cep
+        $avaliaCep = ereg("^[0-9]{8}$", $cep);
+        
+        // verifica o resultado
+        if(!$avaliaCep) {            
+            return "false";
+        }
+        else{
+            return "true";
         }
     }
 
