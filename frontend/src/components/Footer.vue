a2d8d162fd84030d6f5237cb6523920a3f18dbf8
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-09-30 10:36:10 -0300

    alterando estado footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,9 +1,20 @@
 <template>
   <footer>
-    <div class="container-fluid">
+    <!-- <div class="container-fluid">
       <row>
         <p class="copyright"> &copy; Banco de Currículos 2019</p>
       </row>
+    </div> -->
+    <div class="mini-footer">
+      <div class="containerFooter">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="copyright-text">
+              <p> &copy; Banco de Currículos 2019</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
