f2a67a9c7c23cef294ffc28672dc361520cb380a
krdg10 <krdg10@live.com>
2019-10-01 10:01:16 -0400

    vaga com validator e validação update

--- backend/app/Http/Controllers/VagaController.php
+++ backend/app/Http/Controllers/VagaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use Response;
 
@@ -37,82 +38,11 @@ class VagaController extends Controller
 
     public function store(Request $request)
     {   
-        if(!$request->titulo){
-            $error[] = 'Insira um titulo!';
-        }
-        else {
-            if(strlen($request->titulo)>50){
-                $error[] = 'Insira título com no máximo 50 caracteres!';
-            }
-        }
-
-        if(!$request->local){
-            $error[] = 'Insira um local!';
-        }
-        else {
-            if(strlen($request->local)>50){
-                $error[] = 'Insira local com no máximo 50 caracteres!';
-            }
-        }
-
-        if(!$request->quantidade){
-            $error[] = 'Insira a quantidade!';
-        }else{
-            if (!is_numeric($request->quantidade)){
-                $error[] = 'Insira a quantidade apenas com números!';
-            }
-            else if ($request->quantidade<1){
-                $error[] = 'Insira a quantidade com número maior ou igual a 1!';
-            }
-        }
-
-        if(!$request->area){
-            $error[] = 'Insira uma área!';
-        }
-        
-        
-        if(!$request->salario){
-            $error[] = 'Insira o salário!';
-        }
-        else{
-            if (!is_numeric($request->salario)){
-                $error[] = 'Insira o salário apenas com números!';
-            }
-            else if ($request->salario<1){
-                $error[] = 'Insira o salário com número maior ou igual a 1!';
-            }
-        }
-
-        if(!$request->jornada){
-            $error[] = 'Insira a jornada!';
-        }
-        else{
-            if(strlen($request->jornada)>50){
-                $error[] = 'Insira jornada com no máximo 50 caracteres!';
-            }
-        }
-
-        if(!$request->beneficios){
-            $error[] = 'Insira benefícios!';
-        }
-        else{
-            if(strlen($request->beneficios)>500){
-                $error[] = 'Insira benefícios com no máximo 50 caracteres!';
-            }
-        }
-       
-        if(!$request->requisitos){
-            $error[] = 'Insira os requisitos!';
-        } 
-        else{
-            if(strlen($request->requisitos)>500){
-                $error[] = 'Insira requisitos com no máximo 50 caracteres!';
-            }
-        }
-       
-        if(isset($error)){
-            return Response::json([
-            'error' => $error
+        $validator = Validator::make($request->all(), VagaController::rules(), VagaController::messages());
+         
+        if ($validator->fails()) {
+             return Response::json([
+                'error' => $validator->messages()
             ], 201);
         }
         $juridicas_id = Juridica::where('user_id', $request->user_id)->first()->id;
@@ -150,6 +80,14 @@ class VagaController extends Controller
 
     public function update(Request $request, $id)
     {
+        $validator = Validator::make($request->all(), VagaController::rules(), VagaController::messages());
+         
+        if ($validator->fails()) {
+             return Response::json([
+                'error' => $validator->messages()
+            ], 201);
+        }
+
         Vaga::where('id', $id)->update([
             'titulo' => $request->titulo,
             'quantidade'=>$request->quantidade,
@@ -182,6 +120,40 @@ class VagaController extends Controller
         return Response::json([
             'msg' => 'deletado ok'
          ], 201);
+    }
+    public function messages(){
+        return $messages = [
+            'titulo.required' => 'Insira um título!',
+            'titulo.max' => 'Insira um título com no máximo 50 caracteres!',
+            'local.required' => 'Insira um local!',
+            'local.max' => 'Insira local com no máximo 50 caracteres',
+            'quantidade.required' => 'Insira uma quantidade!',
+            'quantidade.numeric' => 'Insira quantidade apenas com números!',
+            'quantidade.gt' => 'Insira quantidade com valor maior que 0!',
+            'area.required' => 'Insira uma área!',
+            'salario.required' => 'Insira um salário!',
+            'salario.numeric' => 'Insira salário apenas com números!',
+            'salario.gt' => 'Insira salário com valor maior que 0!',
+            'jornada.required' => 'Insira jornada!',
+            'jornada.max' => 'Insira jornada com no máximo 50 caracteres!',
+            'beneficios.required' => 'Insira os benefícios!',
+            'beneficios.max' => 'Insira benefícios com no máximo 500 caracteres!',
+            'requisitos.required' => 'Insira os requisitos!',
+            'requisitos.max' => 'Insira requisitos com no máximo 500 caracteres!'
+        ];
+    }
+
+    public function rules(){
+        return [
+            'titulo' => 'required|max:50',
+            'local' => 'required|max:50',
+            'quantidade' => 'required|numeric|gt:0',
+            'area' => 'required',
+            'salario' => 'required|numeric|gt:0',
+            'jornada' => 'required|max:50',
+            'beneficios' => 'required|max:500',
+            'requisitos' => 'required|max:500'
+        ];
     }
     
 }
