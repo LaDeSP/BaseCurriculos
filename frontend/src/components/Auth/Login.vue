1f5d1f16a058aa8bc846eb01d38bf7d5e0c4efd7
krdg10 <krdg10@live.com>
2019-10-08 13:51:55 -0400

    vee validate modal de login

--- frontend/src/components/Auth/Login.vue
+++ frontend/src/components/Auth/Login.vue
@@ -1,36 +1,39 @@
 <template>
-<div class="container">
-<div class="row justify-content-center" >
-    <div class="col-sm-8">
-        <div v-if="notificacoes">
-              <span v-for="notificacao in notificacoes" :key="notificacao[0]" class="badge badge-danger badge-pill">
-                  {{notificacao[0]}}
-              </span>
-          </div>
-            <div class="form-group input-group margin-bottom-sm">
-                <span class="input-group-text"><i class="fas fa-envelope fa-fw"></i></span>
-                <input placeholder="Email" type="email"  name="email" class="form-control" required = "autofocus" v-model="email">
-            </div>
-
-            <div class="form-group input-group">
-                <span class="input-group-text"><i class="fa fa-key fa-fw"></i></span>
-                <input placeholder="Senha" type="password" name="password"
-                class="form-control" v-model="password" required = "autofocus">
-            </div>
-
+    <div class="container">
+        <div class="row justify-content-center" >
+            <ValidationObserver v-slot="{ invalid }">
+                <div class="col-sm-8">
+                    <div v-if="notificacoes">
+                        <span v-for="notificacao in notificacoes" :key="notificacao[0]" class="badge badge-danger badge-pill">
+                            {{notificacao[0]}}
+                        </span>
+                    </div>
+                    <div class="form-group input-group margin-bottom-sm">
+                        <span class="input-group-text"><i class="fas fa-envelope fa-fw"></i></span>
+                        <ValidationProvider name="email" rules="required|email|max:50">
+                            <input placeholder="Email" type="email"  name="email" class="form-control" v-model="email">
+                            <!-- <div slot-scope="{ errors }"><p>{{ errors[0] }}</p></div> -->
+                        </ValidationProvider>
+                    </div>
+                    <div class="form-group input-group">
+                        <span class="input-group-text"><i class="fa fa-key fa-fw"></i></span>
+                        <ValidationProvider name="password" rules="required|min:8|max:30">
+                            <input placeholder="Senha" type="password" name="password" class="form-control" v-model="password">
+                            <!-- <div slot-scope="{ errors }"><p>{{ errors[0] }}</p></div> -->
+                        </ValidationProvider>
+                    </div>
+                </div>
+                <div class="col-sm-3 text-right" style="padding-left: 35px;">
+                    <slot name="icone">
+                        <i class="far fa-user fa-5x login-icon"></i>
+                    </slot>
+                </div>
+                <div class="fix-form-modal text-center">
+                    <button :disabled="invalid" @click.prevent="login" type="submit" class="btn btn-lg btn-primary">Entrar</button>
+                </div>
+            </ValidationObserver>
+        </div>
     </div>
-            <div class="col-sm-3 text-right" style="padding-left: 35px;">
-              <slot name="icone">
-                <i class="far fa-user fa-5x login-icon"></i>
-              </slot>
-            </div>
-            <div class="fix-form-modal text-center">
-              <button @click.prevent="login" type="submit" class="btn btn-lg btn-primary">Entrar</button>
-            </div>
-
-
-</div>
-</div>
 </template>
 
 <script>
@@ -42,7 +45,6 @@
                 email: '',
                 password: '',
                 notificacoes: [],
-
             }
         },
         methods: {
