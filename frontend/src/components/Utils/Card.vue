51c391d2384aec07bba71134e321ef48ee8aaa6b
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-10-09 15:44:53 -0300

    adicionando-slot-card

---
+++ frontend/src/components/Utils/Card.vue
@@ -0,0 +1,27 @@
+<template>
+
+<div class="card">
+  <div class="card-header">
+    <slot name="card-header">
+    </slot>
+  </div>
+  <div class="card-body">
+    <slot name="card-body">
+    </slot>
+    <div class="card-footer">
+      <slot name="card-footer">
+      </slot>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'card',
+  }
+
+</script>
+
