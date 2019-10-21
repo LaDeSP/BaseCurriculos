23c102b76373a5401364fdecbe907a17ddfe1671
luizamazo <luizamazo@hotmail.com>
2019-10-21 17:49:40 -0400

    list fisica

---
+++ frontend/src/components/Utils/List.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="list-group">
+        <div class="list-group-item  flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+            <slot name="list-header">
+                <h5 class="mb-1">List group item heading</h5>
+                <small>3 days ago</small>
+            </slot>
+            </div>
+            <slot name="list-body">
+                <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                <small>Donec id elit non mi porta.</small>
+            </slot>
+            <hr>
+            <slot name="list-footer">
+            </slot>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'list',
+  }
+
+</script>
+
