a0b856d69e63a1552c4589ec91e9369cad7f234f
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-10-21 18:08:58 -0300

    desfazendo

--- frontend/src/components/Lists/Vagas.vue
+++ frontend/src/components/Lists/Vagas.vue
@@ -3,121 +3,71 @@
     <div class="col-md-9">
       <div class="row">
         <div class="col-md-10 float-left" >
-          <template v-if="permissaoDoUsuario === 'JURIDICA'">
-            <div class="panel-heading"><h2>Minhas Vagas</h2></div>
-          </template>
-          <template v-else>
-            <div class="panel-heading"><h2>Vagas</h2></div>
-          </template>
+          <div class="panel-heading"><h2> {{this.displayVagasJuridica}}Minhas Vagas</h2></div>
         </div>
-        <template v-if="permissaoDoUsuario === 'JURIDICA'">
-          <div class="col-md-2 float-right" >
-            <div class="btn-group btn-group-sm">
-              <button @click="onCreate" type="button" class="btn btn-primary"><h4>Criar Vaga <span><i class="fa fa-plus"></i></span></h4></button>
-            </div>
+        <div class="col-md-2 float-right" >
+          <div class="btn-group btn-group-sm">
+            <button @click="onCreate" type="button" class="btn btn-primary"><h4>Criar Vaga <span><i class="fa fa-plus"></i></span></h4></button>
           </div>
-        </template>
+        </div>
       </div>
       <br>
-      <template v-if="permissaoDoUsuario === 'JURIDICA'">
-        <div class="d-flex flex-row bd-highlight mb-3">
-          <div class="p-2 bd-highlight">
-            <div class="btn-group btn-group-sm" role="group">
-                <button @click="changeActiveButton('ativa')" type="button" class="active btn btn-outline-success">Vagas Ativas</button>
-                <button @click="changeActiveButton" type="button" class="btn btn-outline-secondary">Vagas Inativas</button>
-            </div>
+      <div class="d-flex flex-row bd-highlight mb-3">
+        <div class="p-2 bd-highlight">
+          <div class="btn-group btn-group-sm" role="group">
+              <button @click="changeActiveButton('ativa')" type="button" class="active btn btn-outline-success">Vagas Ativas</button>
+              <button @click="changeActiveButton" type="button" class="btn btn-outline-secondary">Vagas Inativas</button>
           </div>
         </div>
-      </template>
+      </div>
       <div class="row">
           <div  v-for="vaga in isActive" :key="vaga.id" :id="vaga.id" @vagaDeleted="onVagaDeleted($event)">
-            <template v-if="permissaoDoUsuario === 'JURIDICA'">
-              <Card style="width: 30rem;">
-                <template v-slot:card-header>
-                  <h3><span class="label label-info ">{{vaga.titulo}}</span></h3>
-                </template>
-                <template v-slot:card-body>
-                  <p>Cargo:{{vaga.local}}</p>
-                    <p>Status:{{vaga.status}}</p>
-                  <p>Quantidade: {{vaga.quantidade}}</p>
-                  <p>Área de Atuação: {{vaga.area.tipo}}</p>
-                  <p>Salário: {{vaga.salario}}</p>
-                  <p>Jornada de Trabalho: {{vaga.jornada}}</p>
-                  <p>Benefícios: {{vaga.beneficio}}</p>
-                  <p>Requisitos: {{vaga.requisito}}</p>
-                </template>
-                <template v-slot:card-footer>
-                  <div>
-                    <button @click="onEdit(vaga.id)" class="btn btn-sm btn-warning">Editar</button>
-                    <span v-if="filterState">
-                        <button @click="changeStatus(vaga.id, 'INATIVA')" class="btn btn-sm btn-outline-secondary">Desativar</button>
-                    </span>
-                    <span v-else>
-                        <button @click="changeStatus(vaga.id, 'ATIVA')" class="btn btn-sm btn-outline-success">Ativar</button>
-                    </span>
-                    <button @click="showModal('warning')" class="btn btn-sm btn-danger">Deletar</button>
-                    <Modal v-show="isModalWarning" @close="closeModal">
-                        <template v-slot:header><h3>Deletar Vaga</h3></template>
-                        <template v-slot:body>
-                            <h2 class="text-center">Tem certeza de que deseja
-                                <span style="color: #ff0000"><strong>deletar</strong></span>
-                                essa vaga?</h2>
-
-                        </template>
-                        <template v-slot:footer>
-                        <div class="modal-footer">
-                            <button @click="onDelete(vaga.id)" class="btn btn-lg btn-danger">Sim</button>
-                            <button @click="closeModal" class="btn btn-lg btn-success">Não</button>
-                        </div>
-                        </template>
-                    </Modal>
-                  </div>
-                </template>
-              </Card>
-            </template>
-            <template v-else>
-              <List>
-                <template v-slot:list-header>
-                    <h3 class="mb-1">{{vaga.titulo}}</h3>
-                </template>
-                <template v-slot:list-body>
-                    <p class="mb-1">Cargo:{{vaga.local}}</p>
-                    <p class="mb-1">Área de Atuação: {{vaga.area.tipo}}</p>
-                    <p class="mb-1">Jornada de Trabalho: {{vaga.jornada}}</p>
-                    <p class="mb-1">Salário: {{vaga.salario}}</p>
-                    <p class="mb-1">Benefícios: {{vaga.beneficio}}</p>
-                    <p class="mb-1">Requisitos: {{vaga.requisito}}</p>
-                </template>
-                <template v-slot:list-footer>
-                  <button @click="showModal" class="btn btn-sm btn-default">Ver mais</button>
-                  <Modal v-show="isModalShowMore" @close="closeModal">
-                        <template v-slot:header><h3>Detalhes da Vaga</h3></template>
-                        <template v-slot:body>
-                          <h3 class="mb-1">{{vaga.titulo}}</h3>
-                          <p class="mb-1">Cargo:{{vaga.local}}</p>
-                          <p class="mb-1">Área de Atuação: {{vaga.area.tipo}}</p>
-                          <p class="mb-1">Jornada de Trabalho: {{vaga.jornada}}</p>
-                          <p class="mb-1">Salário: {{vaga.salario}}</p>
-                          <p class="mb-1">Benefícios: {{vaga.beneficio}}</p>
-                          <p class="mb-1">Requisitos: {{vaga.requisito}}</p>
-                        </template>
-                        <template v-slot:footer>
-                        <div class="modal-footer">
-                            <button @click="onDelete(vaga.id)" class="btn btn-lg btn-danger">Sim</button>
-                            <button @click="closeModal" class="btn btn-lg btn-success">Não</button>
-                        </div>
-                        </template>
-                  </Modal>
-                  <div v-if="a">
+            <Card style="width: 30rem;">
+              <template v-slot:card-header>
+                <h3><span class="label label-info ">{{vaga.titulo}}</span></h3>
+              </template>
+              <template v-slot:card-body>
+                <p>Local:{{vaga.local}}</p>
+                  <p>Status:{{vaga.status}}</p>
+                <p>Quantidade: {{vaga.quantidade}}</p>
+                <p>Área de Atuação: {{vaga.area.tipo}}</p>
+                <p>Salário: {{vaga.salario}}</p>
+                <p>Jornada de Trabalho: {{vaga.jornada}}</p>
+                <p>Benefícios: {{vaga.beneficio}}</p>
+                <p>Requisitos: {{vaga.requisito}}</p>
+              </template>
+              <template v-slot:card-footer>
+                <div v-if="permissaoDoUsuario === 'FISICA'">
+                    <router-link v-bind:to="'' + vaga.id" tag="button" class="btn btn-sm btn-default">Ver mais</router-link>
                     <button @click="onRequest(vaga.id)" class="btn btn-sm btn-success">Se Candidatar</button>
-                  </div>
-                  <div v-else>
-                    <button @click="a" class="btn btn-sm btn-warning disabled">Complete seu currículo para se candidatar!</button>
-                  </div>
-                  
-                </template>
-              </List>
-            </template>
+                </div>
+                <div v-else>
+                  <button @click="onEdit(vaga.id)" class="btn btn-sm btn-warning">Editar</button>
+                  <span v-if="filterState">
+                      <button @click="changeStatus(vaga.id, 'INATIVA')" class="btn btn-sm btn-outline-secondary">Desativar</button>
+                  </span>
+                  <span v-else>
+                      <button @click="changeStatus(vaga.id, 'ATIVA')" class="btn btn-sm btn-outline-success">Ativar</button>
+                  </span>
+                  <button @click="showModal" class="btn btn-sm btn-danger">Deletar</button>
+                  <Modal v-show="isModalWarning" @close="closeModal">
+                      <template v-slot:header><h3>Deletar Vaga</h3></template>
+                      <template v-slot:body>
+                          <h2 class="text-center">Tem certeza de que deseja
+                              <span style="color: #ff0000"><strong>deletar</strong></span>
+                              essa vaga?</h2>
+
+                      </template>
+                      <template v-slot:footer>
+                      <div class="modal-footer">
+                          <button @click="onDelete(vaga.id)" class="btn btn-lg btn-danger">Sim</button>
+                          <button @click="closeModal" class="btn btn-lg btn-success">Não</button>
+                      </div>
+                      </template>
+                  </Modal>
+                </div>
+              </template>
+            </Card>
           </div>
         </div>
     </div>
@@ -127,7 +77,6 @@
 
 <script>
   import Card from '../Utils/CardsVagas';
-  import List from '../Utils/List';
   import Modal from '../Utils/Modal';
   import { mapActions, mapGetters } from 'vuex';
 
@@ -138,27 +87,20 @@
             vagas: [],
             filterState: true,
             isModalWarning: false,
-            isModalShowMore: false,
         }
     },
-    components: {Card, Modal, List},
+    components: {Card, Modal},
         methods: {
           ...mapActions([
               'loadVagasJuridica'
           ]),
 
-          showModal(modal){
-            if(modal === 'warning'){
+          showModal(){
               this.isModalWarning = true;
-            }else{
-              this.isModalShowMore = true;
-            }
           },
 
           closeModal(){
               this.isModalWarning = false;
-              this.isModalShowMore = false;
-              console.log('entro')
           },
 
           onCreate(){
@@ -261,9 +203,19 @@
             },
 
             ...mapGetters([
-              'displayVagasJuridica', 'permissaoDoUsuario', 'dataCompleted'
+              'displayVagasJuridica', 'permissaoDoUsuario'
             ]),
 
+        },
+
+         watch: {
+          '$store.state.vagasJuridica':{
+            handler() {
+
+            },
+
+            deep: true
+          }
         },
 
         created(){
