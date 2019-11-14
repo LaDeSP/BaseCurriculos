172e29d86946f6de3ba8d31e2dd08003affe9a33
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-11-14 18:11:00 -0300

    modficando-botõesdeVoltar-Navbar-retirandonotificações

--- frontend/src/components/Lists/Curriculo.vue
+++ frontend/src/components/Lists/Curriculo.vue
@@ -4,7 +4,7 @@
       <div class="row justify-content-center">
         <div div class="col-12">
           <div class="btn-group">
-            <router-link to="/DashFIS" class="btn btn-primary btn-lg btn-block"><i class="fas fa-arrow-left"></i>Voltar</router-link>
+            <router-link to="/dashboard-fisica" class="btn btn-primary btn-lg btn-block"><i class="fas fa-home"></i> Home</router-link>
           </div>
         </div>
       </div>
--- frontend/src/components/Utils/NavBar.vue
+++ frontend/src/components/Utils/NavBar.vue
@@ -7,62 +7,10 @@
       <div class="container">
           <busca></busca>
       </div>
-			
+
       <!-- Topbar Navbar -->
       <ul class="navbar-nav ml-auto">
 
-        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-        <li class="nav-item dropdown no-arrow d-sm-none">
-          <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-search fa-fw"></i>
-          </a>
-        </li>
-        <!-- Nav Item - Alerts -->
-        <li class="nav-item dropdown no-arrow mx-1">
-          <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-bell fa-fw"></i>
-          <!-- Counter - Alerts -->
-            <span class="badge badge-counter">0</span>
-          </a>
-          <!-- Dropdown - Alerts -->
-          <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
-            <h6 class="dropdown-header">Alertas</h6>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="mr-3">
-                <div class="icon-circle bg-primary">
-                  <i class="fas fa-file-alt text-white"></i>
-                </div>
-              </div>
-              <div>
-                <div class="small text-gray-500">December 12, 2019</div>
-                <span class="font-weight-bold">Você recebeu um novo currículo para a vaga!</span>
-              </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="mr-3">
-                <div class="icon-circle bg-success">
-                  <i class="fas fa-donate text-white"></i>
-                </div>
-              </div>
-              <div>
-                <div class="small text-gray-500">December 7, 2019</div>
-                Um profissional da área que você procura se cadastrou no site.
-              </div>
-            </a>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-              <div class="mr-3">
-                <div class="icon-circle bg-warning">
-                  <i class="fas fa-exclamation-triangle text-white"></i>
-                </div>
-              </div>
-              <div>
-                <div class="small text-gray-500">December 2, 2019</div>
-                Nós notamos uma atividade suspeita na sua conta.
-              </div>
-            </a>
-            <a class="dropdown-item text-center small text-gray-500" href="#">Mostre todos os alertas.</a>
-          </div>
-        </li>
         <div class="topbar-divider d-none d-sm-block"></div>
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
@@ -78,10 +26,6 @@
               <a class="dropdown-item" href="#">
                 <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                 Configurações
-              </a>
-              <a class="dropdown-item" href="#">
-                <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                Atividades
               </a>
               <div class="dropdown-divider"></div>
               <Logout></Logout>
@@ -107,7 +51,7 @@ import busca from './BarraBusca';
             return{
               name: this.$store.state.auth.user.name,
             }
-            
+
         },
     }
 </script>
