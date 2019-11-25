1e555fd191fee3ca0770809b3448c79f92cb8437
krdg10 <krdg10@live.com>
2019-11-25 14:35:18 -0400

    mudança nas cores do modal: com style próprio pq classe não tava funcionando. N aparecia nem no console e ficava smepre o inherit

--- frontend/src/components/Utils/Modal.vue
+++ frontend/src/components/Utils/Modal.vue
@@ -2,13 +2,13 @@
   <div class="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" @click="close">
     <div class="modal-dialog modal-dialog-scrollable" role="document">
       <div class="modal-content" @click.stop>
-        <div class="modal-header">
+        <div class="modal-header" style="color: #858796;">
           <slot name="header">
             <h5 class="modal-title" id="exampleModalLabel" >Eu sou a header.</h5>
           </slot>
           <button data-dismiss="modal" @click="close" class="close" aria-label="Close" style='color: #ff0000;'>x</button>
         </div>
-        <div class="modal-body">
+        <div class="modal-body" style="color: #858796;">
           <slot name="body">
             Sou o body.
           </slot>
