1f9df6bb902f4d641d387d62ab22aac003df21a3
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-12-14 00:49:06 -0300

    tela de configurações terminada

--- frontend/src/components/Lists/Config.vue
+++ frontend/src/components/Lists/Config.vue
@@ -1,93 +1,103 @@
 <template>
-<div class="col-lg-12">
-  <div class="container-fluid fix-form-modal">
-  <div class="row  justify-content-center">
-      <ValidationObserver v-slot="{ invalid }">
-        <form>
-          <div v-if="notificacoes">
-            <span v-for="notificacao in notificacoes" :key="notificacao[0]" class="badge badge-danger badge-pill">
-                {{notificacao[0]}}
-            </span>
+<div class="row justify-content-center">
+    <div class="col-8">
+          <div class="row mb-3">
+          <div class="col-4">
+            <router-link v-bind:to="'/dashboard/'"  tag="button" class="btn btn-md btn-outline-secondary"><i class="fas fa-home fa-sm"></i> Home</router-link>
           </div>
-         <div class="col-sm-12">
-            <div class="form-group">
-                  <label for="email">Email <a class="color-red">*</a></label>
-                <ValidationProvider name="email" rules="required|email|max:250">
+          <div class="col-6">
+            <h2>Configurações</h2>
+          </div>
+        </div>
+    <div class="container-fluid fix-form-modal">
+    <div class="row  justify-content-center">
+      <div class="card">
+        <div class="card-body">
+          <ValidationObserver v-slot="{ invalid }">
+            <form>
+              <div v-if="notificacoes">
+                <span v-for="notificacao in notificacoes" :key="notificacao[0]" class="badge badge-danger badge-pill">
+                    {{notificacao[0]}}
+                </span>
+              </div>
+            <div class="col-sm-12">
+                <div class="form-group">
+                      <label for="email">Email <a class="color-red">*</a></label>
+                    <ValidationProvider name="email" rules="required|email|max:250">
+                        <div slot-scope="{ errors }">
+                          <input type="email" name="email" class="form-control" v-model="email" maxlength="250" required='autofocus'>
+                          <p class="color-red">{{ errors[0] }}</p>
+                        </div>
+                    </ValidationProvider>
+                </div>
+              </div>
+
+              <div class="col-sm-12" v-if="role=='FISICA'">
+                <div class="form-group">
+                  <label for="cpf">CPF <a class="color-red">*</a></label>
+                  <ValidationProvider name="cpf" rules="required|numeric|digits:11">
+                      <div slot-scope="{ errors }">
+                        <input type="text" class="form-control" name="cpf" v-model="cpf" maxlength="11" minlength="11" required='autofocus'>
+                        <p class="color-red">{{ errors[0] }}</p>
+                      </div>
+                  </ValidationProvider>
+                </div>
+              </div>
+
+              <div class="col-sm-12" v-if="role=='JURIDICA'">
+                <div class="form-group">
+                  <label for="cnpj">CNPJ <a class="color-red">*</a></label>
+                  <ValidationProvider name="cnpj" rules="required|numeric|digits:14">
+                      <div slot-scope="{ errors }">
+                        <input type="text" class="form-control" name="cnpj" v-model="cnpj" maxlength="14" minlength="14" required='autofocus'>
+                        <p class="color-red">{{ errors[0] }}</p>
+                      </div>
+                  </ValidationProvider>
+                </div>
+              </div>
+
+
+              <div class="col-sm-12">
+                <div class="form-group">
+                  <span for="newPassword">Nova Senha</span>
+                  <ValidationProvider name="newPassword" rules="min:8|max:30" vid="newPassword">
                     <div slot-scope="{ errors }">
-                      <input type="email" name="email" class="form-control" v-model="email" maxlength="250" required='autofocus'>
+                      <input type="password" id="newPassword" name="newPassword" class="form-control" v-model="newPassword" maxlength="30" minlength="8">
+                      <small>Só precisa ser preenchido se for trocar de senha. Não é obrigatório.</small>
                       <p class="color-red">{{ errors[0] }}</p>
                     </div>
-                </ValidationProvider>
-            </div>
-          </div>
-
-          <div class="col-sm-12" v-if="role=='FISICA'">
-            <div class="form-group">
-              <label for="cpf">CPF <a class="color-red">*</a></label>
-              <ValidationProvider name="cpf" rules="required|numeric|digits:11">
-                  <div slot-scope="{ errors }">
-                    <input type="text" class="form-control" name="cpf" v-model="cpf" maxlength="11" minlength="11" required='autofocus'>
-                    <p class="color-red">{{ errors[0] }}</p>
-                  </div>
-              </ValidationProvider>
-            </div>
-          </div>
-
-          <div class="col-sm-12" v-if="role=='JURIDICA'">
-            <div class="form-group">
-              <label for="cnpj">CNPJ <a class="color-red">*</a></label>
-              <ValidationProvider name="cnpj" rules="required|numeric|digits:14">
-                  <div slot-scope="{ errors }">
-                    <input type="text" class="form-control" name="cnpj" v-model="cnpj" maxlength="14" minlength="14" required='autofocus'>
-                    <p class="color-red">{{ errors[0] }}</p>
-                  </div>
-              </ValidationProvider>
-            </div>
-          </div>
-
-          
-          <div class="col-sm-12">
-            <div class="form-group">
-              <span for="newPassword">Nova Senha</span>
-              <ValidationProvider name="newPassword" rules="min:8|max:30" vid="newPassword">
-                <div slot-scope="{ errors }">
-                  <input type="password" id="newPassword" name="newPassword" class="form-control" v-model="newPassword" maxlength="30" minlength="8">
-                  <small>Só precisa ser preenchido se for trocar de senha. Não é obrigatório.</small>
-                  <p class="color-red">{{ errors[0] }}</p>
+                  </ValidationProvider>
                 </div>
-              </ValidationProvider>
-            </div>
-          </div>
+              </div>
 
-          <div class="col-sm-12" v-if="newPassword != ''" ><!--isso tava no a -->
-            <div class="form-group">
-              <span for="password">Senha Atual <a class="color-red">*</a></span>
-              <ValidationProvider name="password" rules="required|min:8|max:30"><!--required_if aqui se eele tiver direto sem aquele v-if-->
-                <div slot-scope="{ errors }">
-                  <input type="password" id="password" name="password" class="form-control" :disabled="newPassword==''" v-model="password" maxlength="30" minlength="8">
-                  <!--<small>Só precisa ser preenchido quando há uma nova senha. Não é obrigatório em outros casos. Não causa efeito nenhum se preenchido sem nova senha.</small>-->
-                  <p class="color-red">{{ errors[0] }}</p>
+              <div class="col-sm-12" v-if="newPassword != ''" ><!--isso tava no a -->
+                <div class="form-group">
+                  <span for="password">Senha Atual <a class="color-red">*</a></span>
+                  <ValidationProvider name="password" rules="required|min:8|max:30"><!--required_if aqui se eele tiver direto sem aquele v-if-->
+                    <div slot-scope="{ errors }">
+                      <input type="password" id="password" name="password" class="form-control" :disabled="newPassword==''" v-model="password" maxlength="30" minlength="8">
+                      <!--<small>Só precisa ser preenchido quando há uma nova senha. Não é obrigatório em outros casos. Não causa efeito nenhum se preenchido sem nova senha.</small>-->
+                      <p class="color-red">{{ errors[0] }}</p>
+                    </div>
+                  </ValidationProvider>
                 </div>
-              </ValidationProvider>
-            </div>
+              </div>
+              <div class="row justify-content-center">
+                <button :disabled="invalid" v-on:keyup.enter="register" @click.prevent="update" type="submit" class="btn btn-success btn-lg">Atualizar</button>
+              </div>
+            </form>
+          </ValidationObserver>
           </div>
-
-
-          <hr>
-          <div class="d-flex justify-content-center">
-            <button :disabled="invalid" v-on:keyup.enter="register" @click.prevent="update" type="submit" class="btn btn-success btn-lg">Atualizar</button>
-          </div>
-
-        </form>
-      </ValidationObserver>
-      <Modal v-if="isModalSuccess" @close="closeModal">
-        <template v-slot:header></template>
-        <template v-slot:body>
-              <b-alert show variant="success">
-                <h1>Dados atualizados com sucesso!</h1>
-            </b-alert>
-        </template>
-      </Modal>
+        </div>
+        <Modal v-if="isModalSuccess" @close="closeModal">
+          <template v-slot:header></template>
+          <template v-slot:body>
+                <b-alert show variant="success">
+                  <h1>Dados atualizados com sucesso!</h1>
+              </b-alert>
+          </template>
+        </Modal>
+      </div>
     </div>
   </div>
 </div>
@@ -96,6 +106,7 @@
 </template>
 
 <script>
+  import card from '../Utils/Card';
   import {mapActions} from 'vuex';
   import Modal from '../Utils/ModalOld';
   import { BAlert } from 'bootstrap-vue';
@@ -115,7 +126,7 @@
       }
     },
     components:{
-      Modal, BAlert
+      Modal, BAlert, card
     },
 
     methods: {
@@ -129,7 +140,7 @@
           }
 
           this.$store.dispatch('updateUserFisica', attUserFisica)
-          .then( response => { 
+          .then( response => {
             if(response.error  != undefined){
               this.notificacoes = response.error;
             }
@@ -139,7 +150,7 @@
               this.password = '';
               this.newPassword = '';
             }
-          
+
           })
           .catch(error => console.log(error))
         }
@@ -152,7 +163,7 @@
           }
 
           this.$store.dispatch('updateUserJuridica', attUserJuridica)
-          .then( response => { 
+          .then( response => {
             if(response.error  != undefined){
               this.notificacoes = response.error;
             }
@@ -162,14 +173,14 @@
               this.password = '';
               this.newPassword = '';
             }
-          
+
           })
           .catch(error => console.log(error))
         }
 
       },
       ...mapActions([
-        'loadFisica', 'loadJuridica' 
+        'loadFisica', 'loadJuridica'
       ]),
       closeModal(){
         this.isModalSuccess = false;
