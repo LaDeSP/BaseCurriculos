ad6d77e87dad5e7a52111de9f35c0b40fab925e6
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-12-15 22:01:39 -0300

    pagina de conta desativada concluida

--- frontend/src/components/Auth/ContaDesativada.vue
+++ frontend/src/components/Auth/ContaDesativada.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <h1>Sua conta se encontra desativada no momento. Deseja reativar ela?</h1>
-    <button @click="activate" class="btn btn-lg btn-success">Reativar Conta</button>
-    <button @click="logout" class="btn btn-lg btn-danger">Sair</button>
+  <div class="container">
+      <div class="card">
+        <div class="card-body">
+          <h3>Sua conta se encontra desativada no momento.</h3>
+          <h3>Deseja reativá-la?</h3>
+        </div>
+        <div class="card-footer">
+          <div class="row">
+            <div class="col-6">
+              <div class="d-flex flex-row-reverse"><button @click="activate" class="btn btn-lg btn-success">Reativar Conta</button></div>
+            </div>
+            <div class="col-6">
+              <div class="d-flex flex-row"><button @click="logout" class="btn btn-lg btn-danger">Sair</button></div>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-
     export default {
         methods:{
         activate(){
@@ -16,12 +28,12 @@
                 this.$router.push('/dashboard')
             })
         },
-        logout(){       
+        logout(){
             this.$store.dispatch('logout')
             .then(() => {
                 this.$router.push('/')
             })
-        }  
+        }
         },
     }
 </script>
