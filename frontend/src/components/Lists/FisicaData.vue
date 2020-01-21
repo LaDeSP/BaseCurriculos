e83d2ca371a15714ec95e083ba9489b3e849cb3d
krdg10 <krdg10@live.com>
2020-01-21 10:27:45 -0400

    profile juridica responsivo, uma modificação no fisica

--- frontend/src/components/Lists/FisicaData.vue
+++ frontend/src/components/Lists/FisicaData.vue
@@ -66,10 +66,10 @@
                   <div class="row justify-content-center">
                     <div class="col-md-12">
                       <div class="row">
-                        <div class="col-sm-6" >
+                        <div class="col-sm-6 d-flex justify-content-center" >
                           <router-link to="/new-curriculo" class="btn btn-md btn-warning">Editar Informações</router-link>
                         </div>
-                        <div class="col-sm-6" >
+                        <div class="col-sm-6 d-flex justify-content-center" >
                           <button @click="showModal" class="btn btn-md btn-danger">Desativar Conta</button>
                           <Modal v-show="isModalWarning" @close="closeModal">
                             <template v-slot:header>
